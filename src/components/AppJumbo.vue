d15cd78e70ac591824ad58747ac0020be3bf0c57
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-24 19:27:23 +0100

    feat: add AppNavbar + AppJumbo + AppHeader

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+
+<template>
+    <div class="jumbo">
+
+        <div class="container-centered">
+            <h3>Content Goes here</h3>
+        </div>
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.jumbo {
+    display: flex;
+    align-items: center;
+    background-color: black;
+    height: 100px;
+    width: 100%;
+
+    .container-centered {
+        display: flex;
+        align-items: center;
+        width: 100%;
+    }
+
+    h3 {
+        color: white;
+    }
+}
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,67 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+
+<template>
+    <nav>
+        <div class="container-centered">
+            <div class="logo">
+                <img src="/img/dc-logo.png" alt="logo DC">
+            </div>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+
+
+<style lang="scss" scoped>
+nav {
+    height: 100px;
+    background-color: white;
+
+    .container-centered {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+
+        .logo {
+            display: flex;
+            align-items: center;
+
+            img {
+                width: 70px;
+            }
+        }
+    }
+
+    ul {
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        font-size: .8em;
+        font-weight: bold;
+    }
+}
+</style>
